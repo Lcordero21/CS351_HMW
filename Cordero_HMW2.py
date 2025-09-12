4bcf6f608642bece490ac761200cc3ca98b972ae
Lcordero21 <lcordero@willamette.edu>
2025-09-12 12:03:03 -0700

    wip

--- Cordero_HMW2.py
+++ Cordero_HMW2.py
@@ -7,3 +7,8 @@ def time_algorithm(algo, arr):
     start = time.time()
     algo(arr.copy())
     return time.time() - start
+
+def make_array(n):
+    random.seed(42)
+    final_array = random.sample(range(1,n+1),n)
+    return(final_array)
